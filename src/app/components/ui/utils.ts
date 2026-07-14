b3d4337f492e50c94befb30b07fc53969222af7b
Guillermo Palacin <cguillermop@undac.edu.pe>
2026-07-14 10:48:36 -0500

    Update utils.ts

--- src/app/components/ui/utils.ts
+++ src/app/components/ui/utils.ts
@@ -1,3 +1,7 @@
+// Importa herramientas para combinar clases CSS
+//dinámicas y resolver conflictos de Tailwind.
+// Función que une varias clases CSS y aplica 
+//la última clase de Tailwind cuando existen conflictos.
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
